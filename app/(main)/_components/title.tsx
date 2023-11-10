3223e22cb2acc8841855ff510851ea4f3d94c3a6
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-11-10 18:21:05 +0530

    page content in the making

---
+++ app/(main)/_components/title.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import axios from 'axios'
+import { useParams } from 'next/navigation'
+import { useEffect, useRef, useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+interface TitleProps {
+  initialData: string
+  second: string
+}
+
+const Title = ({ initialData, second }: TitleProps) => {
+  const params = useParams()
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const [title, setTitle] = useState(initialData || 'Untitled')
+  const enableInput = () => {
+    setTitle(initialData)
+    setIsEditing(true)
+    setTimeout(() => {
+      inputRef.current?.focus()
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length)
+    }, 0)
+  }
+
+  const disableInput = () => {
+    setIsEditing(false)
+    if (params.documentId) {
+      axios
+        .patch(`/api/getdocuments/${params.documentId}/updatetitle`, { title })
+        .then((response) => {
+          console.log('Title updated successfully:', response.data)
+        })
+        .catch((error) => {
+          console.error('Error updating title:', error)
+        })
+    }
+  }
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+
+    console.log(event.target.value)
+  }
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      disableInput()
+    }
+  }
+  useEffect(() => {
+    if (!params.documentId) {
+      console.error('Document ID is not defined')
+      return
+    }
+  }, [params.documentId])
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!second && <p>{second}</p>}
+      {isEditing ? (
+        <Input
+          value={title}
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          className="h-7 px-2 focus-visible:ring-transparent"
+        />
+      ) : (
+        <Button
+          onClick={enableInput}
+          value={title}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData}</span>
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export default Title
