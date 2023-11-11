6e5de7cd48ed5017b5f46eb220a7d2c943b440c0
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-11-11 21:02:15 +0530

    adding content page added

---
+++ app/api/Getdocuments/[documentId]/addcontent/route.ts
@@ -0,0 +1,47 @@
+import { db } from '@/lib/db';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from "next/headers";
+import { NextResponse } from 'next/server';
+export async function PATCH(
+    request: Request,
+    { params }: { params: { documentId: number } }
+) {
+
+
+    try {
+        const supabase = createRouteHandlerClient({
+            cookies
+        }); const {
+            data: { user }
+        } = await supabase.auth.getUser();
+
+
+        if (!user) {
+            return new NextResponse("Unauthorised", { status: 401 })
+        }
+        const userId = user?.id
+        if (!userId) {
+            throw new Error("Unauthorised")
+        }
+        const documentId = parseInt(params.documentId?.toString(), 10)
+        const {content} = await request.json()
+        const document = await db.document.update({
+            where: {
+                id: documentId,
+
+                isArchived: false
+            },
+            data: {
+               
+                content:content
+            }
+
+        });
+
+
+        return NextResponse.json(document);
+    } catch (err: any) {
+        console.log(err);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
