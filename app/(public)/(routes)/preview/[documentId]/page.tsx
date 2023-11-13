fd7e0626e97376478a423035e790a10e73570659
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-11-13 09:18:26 +0530

    Get published route removed

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -6,7 +6,7 @@ import { Spinner } from '@/components/spinner'
 import Toolbar from '@/components/toolbar'
 import { useDebounce } from '@/hooks/use-debounce'
 import { createClient } from '@supabase/supabase-js'
-import axios from 'axios'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 interface DocumentIdPageProps {
@@ -22,20 +22,26 @@ const supabase = createClient(
 const DocumentPage = ({ params }: DocumentIdPageProps) => {
   const [data, setData] = useState<any>()
   const [loading, setLoading] = useState(true)
-  const [content, setContent] = useState<string | undefined>(data?.content);
-  const debouncedContent = useDebounce(content, 500); 
+  const [content, setContent] = useState<string | undefined>(data?.content)
+  
   const fetchData = async () => {
     try {
-      const req = await axios.get(`/api/getPublished/${params.documentId}`)
-      const newData = req.data
-      setData(newData)
+      const { data, error } = await supabase
+        .from('Document')
+        .select('*')
+        .eq('id', params.documentId)
+        .eq('isPublished', true)
+        .eq('isArchived', false)
+      if (error) {
+        throw error
+      }
+      setData(data ?? [])
       setLoading(false)
     } catch (error) {
       console.error('Error fetching data:', error)
     }
   }
- 
- 
+
   useEffect(() => {
     fetchData()
     const subscription = supabase
@@ -56,25 +62,34 @@ const DocumentPage = ({ params }: DocumentIdPageProps) => {
       subscription.unsubscribe()
     }
   }, [params.documentId])
- 
-  if(data===null){
-    return <div className=' items-center flex justify-center h-full'>
-        This page was Unpublished by Owner
-    </div>
- }
+
+  if (!data || data.length === 0) {
+    return (
+      <div className=" items-center flex flex-col justify-center h-full text-center">
+        <p>This page was Unpublished by Owner </p>
+        <Link href={'https://notesjunction.vercel.app'}>
+          <p className="hover:underline cursor-pointer">
+            Go to NotesJunction and Create and publish your own note
+          </p>
+        </Link>
+      </div>
+    )
+  }
   return (
     <>
       {loading ? (
-       <div className=' items-center flex justify-center h-full'><Spinner size="lg"/></div>
+        <div className=" items-center flex justify-center h-full">
+          <Spinner size="lg" />
+        </div>
       ) : (
         <div className="pb-40">
-          <Cover preview url={data?.coverImage}/>
+          <Cover preview url={data[0]?.coverImage} />
           <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-            <Toolbar preview initialData={data} />
+            <Toolbar preview initialData={data[0]} />
             <Editor
-            editable={false}
-            onChange={()=>{}}
-            initialContent={data?.content}
+              editable={false}
+              onChange={() => {}}
+              initialContent={data[0]?.content}
             />
           </div>
         </div>
